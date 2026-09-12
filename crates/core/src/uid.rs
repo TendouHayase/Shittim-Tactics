2b3fd2f73f3b889e0842ca61c42dcbb6cf139b9a
TendouHayase <tendouhayase@gmail.com>
2026-09-12 18:28:03 +0900

    feat: impl display for uid

--- crates/core/src/uid.rs
+++ crates/core/src/uid.rs
@@ -1,8 +1,16 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Uid(u64);
 
 impl Uid {
     pub fn new(uid: u64) -> Self {
         Uid(uid)
+    }
+}
+
+impl Display for Uid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
     }
 }
