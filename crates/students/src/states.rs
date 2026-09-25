3d017626c2c027c12ea4f30e9552699bb131c0bf
TendouHayase <me@tendouhayase.xyz>
2026-09-25 13:03:29 +0900

    feat: remove unused file

--- crates/students/src/states.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, Clone, Default)]
-pub struct KeiState {
-    /// Damage stored by the sub skill.
-    pub acc_damage: u64,
-
-    /// Length of the boss damage log when recording started.
-    pub recording_start_len: usize,
-}
